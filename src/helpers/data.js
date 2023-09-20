f0070031d32da1569943cd1ff9836d5751b5b824
Muhammed Said USLU <msaiduslu@gmail.com>
2023-09-20 13:47:27 +0300

    rearranged reade file and photo links

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -2,104 +2,96 @@ export const data = [
   {
     id: 1,
     title: "Fernando Muslera",
-    date:"Kaleci",
-    desc:
-      "2011 yılında Lazio'dan ayrılarak Galatasaray'ın kapısından girdi. Zoran Simoviç, Faryd Mondragon, Claudio Taffarel gibi onun da Galatasaray'da efsane olacağını düşünenlerin sayısı hiç de az değildi. Beklentileri boşa çıkarmadı. Muslera teker teker kupaları kaldırmakla kalmadı; yabancı oyuncular kategorisinde rekora çoktan koşmaya başladı... Toplamda kazandığı 14 kupa ile Galatasaray formasıyla en çok kupa sevinci yaşayan yabancı oyuncu olarak zirvede bulunuyor.",
+    date: "Kaleci",
+    desc: "2011 yılında Lazio'dan ayrılarak Galatasaray'ın kapısından girdi. Zoran Simoviç, Faryd Mondragon, Claudio Taffarel gibi onun da Galatasaray'da efsane olacağını düşünenlerin sayısı hiç de az değildi. Beklentileri boşa çıkarmadı. Muslera teker teker kupaları kaldırmakla kalmadı; yabancı oyuncular kategorisinde rekora çoktan koşmaya başladı... Toplamda kazandığı 14 kupa ile Galatasaray formasıyla en çok kupa sevinci yaşayan yabancı oyuncu olarak zirvede bulunuyor.",
     image:
-      "https://hlkiurt3.rocketcdn.com/profiles/ec51bdd1299b4e0bb3a668d07cfb19ee.jpeg",
+      "https://hlkiurt3.rocketcdn.com/profiles/b13b9abd2c5e41a1a6d69d21e2bb2cce.jpeg",
   },
   {
     id: 2,
     title: "Leo Dubois",
-    date:"Sol Defans",
-    desc:
-      "14 Eylül 1994 yılında Fransa’nın Segré bölgesinde dünyaya gelen Dubois, 2000 yılında başladığı futbol yaşamında 2009’da Nantes’a geçiş yaparak önemli bir adım attı. Nantes akademisinde yetişen ve 2014 yılında A takıma yükselen Fransız sağ bek, dört sezon Nantes ile Ligue 1 forması giydi. 106 maçta 4 gol ve 13 asistle Nantes kariyerini noktaladı. ",
+    date: "Sol Defans",
+    desc: "14 Eylül 1994 yılında Fransa’nın Segré bölgesinde dünyaya gelen Dubois, 2000 yılında başladığı futbol yaşamında 2009’da Nantes’a geçiş yaparak önemli bir adım attı. Nantes akademisinde yetişen ve 2014 yılında A takıma yükselen Fransız sağ bek, dört sezon Nantes ile Ligue 1 forması giydi. 106 maçta 4 gol ve 13 asistle Nantes kariyerini noktaladı. ",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/547def00b3644a00ac5702981327a990.jpeg",
   },
   {
     id: 3,
     title: "Victor Enok Nelsson",
-    date:"Defans",
-    desc:
-      "4 Ekim 1998 yılında Danimarka’nın Hornbæk şehrinde dünyaya gelen Nelsson, ülkesinin Nordsjælland takımının altyapısında futbol yaşamına başladı. Stoper mevkisinde oynayan ve 1.85 metre boyundaki Nelsson, Nordsjælland’da A takıma yükselerek burada 98 resmi maç oynadı ve 2 gole imza attı.Nelsson 2021-2022 sezonundan bu yana Galatasaray forması giymektedir. ",
-    image: "https://hlkiurt3.rocketcdn.com/profiles/8567488f2b034c45aec71bb61ea61ad6.jpeg",
+    date: "Defans",
+    desc: "4 Ekim 1998 yılında Danimarka’nın Hornbæk şehrinde dünyaya gelen Nelsson, ülkesinin Nordsjælland takımının altyapısında futbol yaşamına başladı. Stoper mevkisinde oynayan ve 1.85 metre boyundaki Nelsson, Nordsjælland’da A takıma yükselerek burada 98 resmi maç oynadı ve 2 gole imza attı.Nelsson 2021-2022 sezonundan bu yana Galatasaray forması giymektedir. ",
+    image:
+      "https://hlkiurt3.rocketcdn.com/profiles/80fe338dc7f14c0784a8f1e63fe16058.jpeg",
   },
   {
     id: 4,
     title: "Abdülkerim Bardakcı",
-    date:"Defans",
-    desc:
-      "Kariyerine Zonguldakspor altyapısında başlayan oyuncumuz, Konyaspor altyapısına transfer oldu. Selçukluspor, Adana Demirspor, Samsunspor, Giresunspor, Denizlispor ve Altay gibi kulüplerde kiralık olarak forma giyen Bardakcı, 2020-2021 Süper Lig sezonunda kulübü Konyaspor’a dönerek sezonu burada geçirdi.Abdülkerim Bardakcı, 2022-2023 yaz transfer sezonunda Galatasaray'a katıldı.   ",
+    date: "Defans",
+    desc: "Kariyerine Zonguldakspor altyapısında başlayan oyuncumuz, Konyaspor altyapısına transfer oldu. Selçukluspor, Adana Demirspor, Samsunspor, Giresunspor, Denizlispor ve Altay gibi kulüplerde kiralık olarak forma giyen Bardakcı, 2020-2021 Süper Lig sezonunda kulübü Konyaspor’a dönerek sezonu burada geçirdi.Abdülkerim Bardakcı, 2022-2023 yaz transfer sezonunda Galatasaray'a katıldı.   ",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/7862a9bb2fbb4f4abecd390ef1c22d70.jpeg",
   },
   {
     id: 5,
     title: "Sacha Boey",
-    date:"Sag Defans",
-    desc:
-      "2000 yılında Fransa’nın Montreuil şehrinde doğan Boey, futbolculuk kariyerine Rennes altyapısında başladı. Kamerun asıllı Fransız futbolcu Rennes B Takımı’nda forma giydikten sonra yükselişini sürdürerek A takımda şans buldu.Sacha Boey, Fransa Milli Takımı alt yaş kategorilerinde U17, U18 ve U20 takımlarında da forma giydi.Boey,  2021-2022 sezonundan bu yana Galatasaray forması giymektedir.",
-    image: "https://hlkiurt3.rocketcdn.com/profiles/930dc318cb3548a89524114e032b5be9.jpeg",
+    date: "Sag Defans",
+    desc: "2000 yılında Fransa’nın Montreuil şehrinde doğan Boey, futbolculuk kariyerine Rennes altyapısında başladı. Kamerun asıllı Fransız futbolcu Rennes B Takımı’nda forma giydikten sonra yükselişini sürdürerek A takımda şans buldu.Sacha Boey, Fransa Milli Takımı alt yaş kategorilerinde U17, U18 ve U20 takımlarında da forma giydi.Boey,  2021-2022 sezonundan bu yana Galatasaray forması giymektedir.",
+    image:
+      "https://hlkiurt3.rocketcdn.com/profiles/930dc318cb3548a89524114e032b5be9.jpeg",
   },
   {
     id: 6,
     title: "Milot Rashica",
-    date:"Sag Kanat",
-    desc:
-      "Almanlar onu 8.1 milyon Euro bonservis bedeliyle kadrosuna kattı. Bremen ekibinde 2,5 sezon geçiren Kosova asıllı Arnavut futbolcu Bundesliga’da 87 maçta 21 gol ve 17 asistle dikkat çekti. Werder Bremen kariyerini İngiltere Premier Lig hayaliyle sona erdiren Rashica, 30 maça çıktığı Norwich City’de bir kez golle buluşabildi.Kanatlarda ve forvet arkasında forma giyebilen 26 yaşındaki hücum oyuncusu, Kosova A Milli Takımı’nın formasını 43 kez sırtına geçirdi ve 8 gol kaydetti. ",
+    date: "Sag Kanat",
+    desc: "Almanlar onu 8.1 milyon Euro bonservis bedeliyle kadrosuna kattı. Bremen ekibinde 2,5 sezon geçiren Kosova asıllı Arnavut futbolcu Bundesliga’da 87 maçta 21 gol ve 17 asistle dikkat çekti. Werder Bremen kariyerini İngiltere Premier Lig hayaliyle sona erdiren Rashica, 30 maça çıktığı Norwich City’de bir kez golle buluşabildi.Kanatlarda ve forvet arkasında forma giyebilen 26 yaşındaki hücum oyuncusu, Kosova A Milli Takımı’nın formasını 43 kez sırtına geçirdi ve 8 gol kaydetti. ",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/dd9cbdb8d56d452184ae41235944ec7e.jpeg",
   },
   {
     id: 7,
     title: "Lucas Torreira",
-    date:"Orta Saha",
-    desc:
-      "11 Şubat 1996’da Uruguay’da Fray Bentos kentinde doğan oyuncu, 18 de Julio ve Montevideo Wanderers gibi ülke kulüplerinde yetişirken 2013-14 sezonunda Pescara altyapısına giderek İtalya kariyerine adım attı.Arsenal’da bir sezon geçirdikten sonra ertesi sezon Atletico Madrid’e, ardından da Fiorentina ile anlaşarak İtalya’ya geri döndü. Fiorentina’da 35 maçta 5 gol ve 2 asistlik performans ortaya koyan başarılı ön libero, Arsenal’a sezon sonu dönüşünün ardından Galatasarayımıza katıldı.  ",
+    date: "Orta Saha",
+    desc: "11 Şubat 1996’da Uruguay’da Fray Bentos kentinde doğan oyuncu, 18 de Julio ve Montevideo Wanderers gibi ülke kulüplerinde yetişirken 2013-14 sezonunda Pescara altyapısına giderek İtalya kariyerine adım attı.Arsenal’da bir sezon geçirdikten sonra ertesi sezon Atletico Madrid’e, ardından da Fiorentina ile anlaşarak İtalya’ya geri döndü. Fiorentina’da 35 maçta 5 gol ve 2 asistlik performans ortaya koyan başarılı ön libero, Arsenal’a sezon sonu dönüşünün ardından Galatasarayımıza katıldı.  ",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/4126b2d60177411aaad31e2c703f322a.jpeg",
   },
   {
     id: 8,
     title: "Sergio Oliveira",
-    date:"Orta Saha",
-    desc:
-      "Sergio Oliveira, 2 Haziran 1992 tarihinde Portekiz'in Santa Maria da Feira'ya bağlı Paços de Brandão beldesinde dünyaya geldi.İlk profesyonel imzasını da 2010 yılında Porto'ya atan başarılı orta saha, deneyim kazanması amacıyla kiralık olarak gönderildi. Portekiz'de Beira-Mar, Penafiel, Paços Ferreira, Belçika'da KV Mechelen, Fransa'da FC Nantes, Yunanistan'da PAOK ve İtalya'da Roma formalarını giyen Oliveira, Serie A ekibi ile UEFA Konferans Ligi'nde şampiyonluğa ulaştı.Sergio Oliveira, 2022-2023 yaz transfer sezonunda Galatasaray'a katıldı.",
-    image: "https://hlkiurt3.rocketcdn.com/profiles/90bbcc657a764eb88cc22eaf6fdcc263.jpeg",
+    date: "Orta Saha",
+    desc: "Sergio Oliveira, 2 Haziran 1992 tarihinde Portekiz'in Santa Maria da Feira'ya bağlı Paços de Brandão beldesinde dünyaya geldi.İlk profesyonel imzasını da 2010 yılında Porto'ya atan başarılı orta saha, deneyim kazanması amacıyla kiralık olarak gönderildi. Portekiz'de Beira-Mar, Penafiel, Paços Ferreira, Belçika'da KV Mechelen, Fransa'da FC Nantes, Yunanistan'da PAOK ve İtalya'da Roma formalarını giyen Oliveira, Serie A ekibi ile UEFA Konferans Ligi'nde şampiyonluğa ulaştı.Sergio Oliveira, 2022-2023 yaz transfer sezonunda Galatasaray'a katıldı.",
+    image:
+      "https://hlkiurt3.rocketcdn.com/profiles/90bbcc657a764eb88cc22eaf6fdcc263.jpeg",
   },
   {
     id: 9,
     title: "Kerem Aktürkoğlu",
-    date:"Sag Kanat",
-    desc:
-      "21 yaşındaki futbolcu Türkiye Milli Takımı'nın U18 ve U19 kategorilerinde de forma giydi. Galatasaray'da ilk sezonunda gösterdiği başarılı performansın ardından Türkiye A Milli Takımı'nın Euro 2020 kafilesine davet edildi; ancak bu turnuvada forma giymedi. 27 Mayıs 2020 tarihinde Azerbaycan ile oynanan dostluk maçında ilk defa A Milli formayı giyme onurunu yaşadı.Muhammed Kerem Aktürkoğlu 2020-2021 sezonundan bu yana Galatasaray forması giymektedir.  ",
+    date: "Sag Kanat",
+    desc: "21 yaşındaki futbolcu Türkiye Milli Takımı'nın U18 ve U19 kategorilerinde de forma giydi. Galatasaray'da ilk sezonunda gösterdiği başarılı performansın ardından Türkiye A Milli Takımı'nın Euro 2020 kafilesine davet edildi; ancak bu turnuvada forma giymedi. 27 Mayıs 2020 tarihinde Azerbaycan ile oynanan dostluk maçında ilk defa A Milli formayı giyme onurunu yaşadı.Muhammed Kerem Aktürkoğlu 2020-2021 sezonundan bu yana Galatasaray forması giymektedir.  ",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/3cbe5347319b4f978510425aefed2851.jpeg",
   },
   {
     id: 10,
     title: "Dries Mertens",
-    date:"Forvet",
-    desc:
-      "6 Mayıs 1987’de Belçika’nın Leuven şehrinde doğan Mertens, futbol hayatına doğduğu kentin yerel kulüplerinden Stade Leuven’de başladı. Gent altyapısında futbolculuk yeteneklerini geliştiren oyuncu, Hollanda yolunu tutarak ikinci lig ekiplerinden Eendrach Aalst ve AGOVV’daki performanslarıyla rüştünü kanıtladı. Kiralık olarak gittiği AGOVV’da 2008 yılında ligin en iyi oyuncusu seçildi.İlk kez 2010 yılında Belçika A Milli Takımı’nda şans bulan yıldız oyuncu, 100 maçta 20 kez fileleri sarstı. 2018 Dünya Kupası’nda elemelerle birlikte kaydettiği 5 golle adından çokça söz ettirdi.",
+    date: "Forvet",
+    desc: "6 Mayıs 1987’de Belçika’nın Leuven şehrinde doğan Mertens, futbol hayatına doğduğu kentin yerel kulüplerinden Stade Leuven’de başladı. Gent altyapısında futbolculuk yeteneklerini geliştiren oyuncu, Hollanda yolunu tutarak ikinci lig ekiplerinden Eendrach Aalst ve AGOVV’daki performanslarıyla rüştünü kanıtladı. Kiralık olarak gittiği AGOVV’da 2008 yılında ligin en iyi oyuncusu seçildi.İlk kez 2010 yılında Belçika A Milli Takımı’nda şans bulan yıldız oyuncu, 100 maçta 20 kez fileleri sarstı. 2018 Dünya Kupası’nda elemelerle birlikte kaydettiği 5 golle adından çokça söz ettirdi.",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/1deb4295e6ae4614802333c402fe4c1b.jpeg",
   },
   {
     id: 11,
     title: "Mauro Icardi",
-    date:"Santrafor",
-    desc:
-      "2019-2020 sezonunda İtalya kariyerine nokta koyan dünyaca ünlü santrfor, Paris Saint Germain’e kiralık olarak transfer oldu ve ertesi sezonda 50 milyon Euro bonservis karşılığında PSG’ye kalıcı olarak katıldı. Icardi Paris temsilcisinde 92 maça çıkarak 38 gol ve 10 asistlik performans sergiledi. PSG’deki döneminde iki lig şampiyonluğu, iki Fransa Kupası ve iki Fransa Süper Kupa şampiyonluğu başarılarını kazandı. Arjantin A Milli Takımı formasını 8 kez sırtına geçiren futbolcu, bir gol kaydetti. ",
-    image: "https://hlkiurt3.rocketcdn.com/profiles/b171e1fa82534894a8b46f8543e6b692.jpeg",
+    date: "Santrafor",
+    desc: "2019-2020 sezonunda İtalya kariyerine nokta koyan dünyaca ünlü santrfor, Paris Saint Germain’e kiralık olarak transfer oldu ve ertesi sezonda 50 milyon Euro bonservis karşılığında PSG’ye kalıcı olarak katıldı. Icardi Paris temsilcisinde 92 maça çıkarak 38 gol ve 10 asistlik performans sergiledi. PSG’deki döneminde iki lig şampiyonluğu, iki Fransa Kupası ve iki Fransa Süper Kupa şampiyonluğu başarılarını kazandı. Arjantin A Milli Takımı formasını 8 kez sırtına geçiren futbolcu, bir gol kaydetti. ",
+    image:
+      "https://hlkiurt3.rocketcdn.com/profiles/b171e1fa82534894a8b46f8543e6b692.jpeg",
   },
   {
     id: 12,
     title: "Okan Buruk",
-    date:"Teknik Direktor",
-    desc:
-      "Galatasaray Futbol Akademisi’nin Türkiye’ye kazandırdığı önemli futbol figürlerinden olan Okan Buruk, futbol hayatına Galatasaray altyapısında başladı. 1992 yılında A takıma yükselen Buruk, UEFA şampiyonluğuna giden 2001 yılına kadar Galatasaray formasını başarıyla terletti.2019-2020 sezonunda Başakşehir takımı ile Süper Lig şampiyonluğu yaşayan 48 yaşındaki teknik adam, Akhisar Belediyespor’da 2017-2018 sezonunda Türkiye Kupası şampiyonluğu sevinci yaşadı.Okan Buruk, 2022-2023 Sezonu'nda Galatasaray Futbol A Takımı Teknik Direktörlüğü görevine getirildi.  ",
+    date: "Teknik Direktor",
+    desc: "Galatasaray Futbol Akademisi’nin Türkiye’ye kazandırdığı önemli futbol figürlerinden olan Okan Buruk, futbol hayatına Galatasaray altyapısında başladı. 1992 yılında A takıma yükselen Buruk, UEFA şampiyonluğuna giden 2001 yılına kadar Galatasaray formasını başarıyla terletti.2019-2020 sezonunda Başakşehir takımı ile Süper Lig şampiyonluğu yaşayan 48 yaşındaki teknik adam, Akhisar Belediyespor’da 2017-2018 sezonunda Türkiye Kupası şampiyonluğu sevinci yaşadı.Okan Buruk, 2022-2023 Sezonu'nda Galatasaray Futbol A Takımı Teknik Direktörlüğü görevine getirildi.  ",
     image:
       "https://hlkiurt3.rocketcdn.com/profiles/b2a3780210604f23980f654fbcd46aa0.jpeg",
   },
